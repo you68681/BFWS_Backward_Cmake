9fedc4598cc8119d40206e845910c0ea141ee868
root <root@localhost.localdomain>
2020-05-04 00:03:27 +0800

    update benchmark

--- bench_mark.py
+++ bench_mark.py
@@ -1,6 +1,8 @@
+
 import  os
 import csv
 import re
+'''
 files_list=[]
 path = "/home/chao/LAPKT-public/benchmarks/ipc-2011/blocksworld/instances"
 files = os.listdir(path)
@@ -73,3 +75,104 @@ write(out_put_list,"./blocksworld_result/result_left.csv")
 
 #for line in result.split("\n"):
 #     if "Total time" in line:
+'''
+import time
+import subprocess
+
+def cmd(command,result_dict,file):
+    result_dict[file]=dict()
+    subp = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
+    try:
+        subp.wait(600)
+    except:
+            result_dict[file]["Plan found with cost"] = "--"
+            result_dict[file]["Total time"] = "--"
+            result_dict[file]["Nodes generated during search"] = "--"
+            result_dict[file]["Nodes expanded during search"] = "--"
+            result_dict[file]["Fast-BFS search completed in"] = "--"
+            result_dict[file]["Goals found"] = "--"
+            result_dict[file]["Goals_Edges found"] = "--"
+            result_dict[file]["Plan found with cost"] = "no plan"
+    if subp.poll() == 0:
+        for line in (subp.communicate()[0]).split("\n"):
+            if "Plan found with cost" in line:
+                result_dict[file]["Plan found with cost"] = line.split(":")[-1]
+            if "Total time" in line:
+                result_dict[file]["Total time"] = line.split(":")[-1]
+            if "Nodes generated during search" in line:
+                result_dict[file]["Nodes generated during search"] = line.split(":")[-1]
+            if "Nodes expanded during search" in line:
+                result_dict[file]["Nodes expanded during search"] = line.split(":")[-1]
+            if "Fast-BFS search completed in" in line:
+                result_dict[file]["Fast-BFS search completed in"] = " ".join(line.split(" ")[-2:])
+            if "Goals found" in line:
+                result_dict[file]["Goals found"] = line.split(":")[-1]
+            if "Goals_Edges found" in line:
+                result_dict[file]["Goals_Edges found"] = line.split(":")[-1]
+            if "Plan found with cost" not in result_dict[file]:
+                result_dict[file]["Plan found with cost"] = "no plan"
+    else:
+        result_dict[file]["Plan found with cost"] = "time out"
+        result_dict[file]["Total time"] = "time out"
+        result_dict[file]["Nodes generated during search"] = "time out"
+        result_dict[file]["Nodes expanded during search"] = "time out"
+        result_dict[file]["Fast-BFS search completed in"] = "time out"
+        result_dict[file]["Goals found"] = "time out"
+        result_dict[file]["Goals_Edges found"] = "time out"
+        result_dict[file]["Plan found with cost"] = "no plan"
+    return result_dict
+
+
+
+
+def write(output_list, output_file):
+
+    with open(output_file, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["file name", "Goals found","Goals_Edges found","Plan found with cost", "Total time", "Nodes generated during search", "Nodes expanded during search",
+                  "Fast-BFS search completed in"])
+        for row in output_list:
+            writer.writerow(row)
+
+def run_bfws(domain):
+    subprocess.Popen("mkdir /home/ubuntu/new-sample-pddl-results/"+domain, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+    path = os.path.join("/home/ubuntu/new-pddl-domains",domain,"instances")
+    domain_path=os.path.join("/home/ubuntu/new-pddl-domains",domain,"domain.pddl")
+    files = os.listdir( path)
+
+    result_dict=dict()
+    files.sort()
+    for file in files:
+        print(file)
+        cmd("/xenon/BFWS_Backward_Cmake/cmake-build-debug/bfws --domain "+ domain_path+" --problem "+os.path.join(path,file)+" --output /home/ubuntu/new-sample-pddl-results/"+domain+"/" + file + "_result.txt --max_novelty 2 --k-BFWS true",result_dict,file)
+    result_out_put = {}
+
+    out_put_list = []
+
+    result_dict_keys = sorted(result_dict.keys())
+    for key in result_dict_keys:
+        temp_list = []
+        for i in ["Goals found", "Goals_Edges found", "Plan found with cost", "Total time",
+                      "Nodes generated during search", "Nodes expanded during search",
+                      "Fast-BFS search completed in"]:
+            temp_list.append(result_dict[key][i])
+        result_out_put[key] = temp_list
+
+    for key in result_out_put.keys():
+        temp_list = []
+        temp_list.append(key)
+        for i in range(len(result_out_put[key])):
+            temp_list.append(result_out_put[key][i])
+        out_put_list.append(temp_list)
+
+    write(out_put_list,os.path.join("/home/ubuntu/new-sample-pddl-results",domain,"result.csv"))
+
+def start():
+    files = os.listdir("/home/ubuntu/new-pddl-domains")
+    for file in files:
+        run_bfws(file)
+
+
+
+if __name__ == "__main__":
+    start()
